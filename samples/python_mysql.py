a77648bf7e9dce85a4353d7d9303082cc9524188
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-04-03 06:06:52 +0000

    add

--- samples/python_mysql.py
+++ samples/python_mysql.py
@@ -2,7 +2,8 @@ import pymysql
 
 # 데이터베이스 연결 설정
 conn = pymysql.connect(
-    host='python_mysql_mysql',  # 컨테이너 이름 또는 IP
+    host='192.168.10.236',  # 컨테이너 이름 또는 IP
+    port = 3306,
     user='cocolabhub',
     password='cocolabhub',
     db='python_mysql',  # 데이터베이스 이름
